aa23f07e9cd87cebaaf9834c1ef2aa7eeaa5ed81
Yumeng Liu <lym20041026@gmail.com>
2026-05-16 14:28:43 -0700

    fix import

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,8 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
-from models.response_models import AnalyzeResponse
-from services import cache_service
 from common.constants import ALLOWED_ORIGINS
-from routes.analyze import router as analyze_router
-
+from routes.sessions import session_router
 
 load_dotenv()
 
@@ -24,7 +20,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(analyze_router)
+app.include_router(session_router)
 
 
 @app.get("/health")
